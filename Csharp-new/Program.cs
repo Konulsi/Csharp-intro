d7ba15138ba9826da738b2bf7c3348d4a23d49d1
konulsi <konulsi@code.edu.az>
2022-12-08 02:34:16 +0300

    new file

---
+++ Csharp-new/Program.cs
@@ -0,0 +1,108 @@
+﻿
+
+//Task:1- Verilmish n - ededinin 3 - e ve 7 - e bolunub - bolunmemesini tapin.
+
+//int n = 21;
+
+//if (n % 3 == 0 && n % 7 == 0)
+//{
+//    Console.WriteLine("bolunur");
+//}
+
+//else
+//{
+//    Console.WriteLine("bolunmur");
+//} 
+
+
+
+
+
+
+
+//Task: 2 - n və m ededleri verilir. Eger n və m ededleri her ikisi cutdurse n və m ededlerinin cəmini hesablayin.
+
+//int n = 10;
+//int m = 15;
+//int sum = 0;
+
+//if (n % 2 == 0 && m % 2 == 0)
+//{
+//    sum = n + m;
+
+//    Console.WriteLine(sum);
+//}
+
+//else
+//{
+//    Console.WriteLine("duzgun reqem daxil edin");
+//}
+
+
+
+
+
+
+
+//Task: 3 - Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+
+
+//int n = 5;
+//int m = 10;
+//int sum = 0;
+
+//for (int i = n; i < m; i++)
+//{
+//    if ( i % 2 == 1)
+//    {
+//        sum += i;
+//    }
+
+//}
+
+//Console.WriteLine(sum);
+
+
+
+
+
+//Task:4- Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+
+
+//int[] numbers= { 1, 3, 4, 5, 9, 11, 14, 20 };
+//int sum = 0;
+
+//for (int i = 0; i < numbers.Length; i++)
+
+//{
+//    if (numbers[i] % 2 == 1)
+//    {
+//        sum += numbers[i];
+//    } 
+
+//}
+//Console.WriteLine(sum);
+
+
+
+
+
+//Task:5- Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+
+//int[] nums = { 1, 2, 3, 4, 6, 8, 11, 12, 16, 19, 22 };
+//int count = 0;
+
+//foreach (var item in nums)
+//{
+//    if (item % 2 == 0)
+//        count++;
+
+
+//}
+
+//Console.WriteLine(count);
+
+
